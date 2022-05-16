87f42d4fcb37dc3f256f2e40bbe9519e5371aeae
Johnny Martinez <mrtnz.jn@gmail.com>
2022-05-16 15:41:44 -0500

    Inside PostController created getAll and getById methods. Both methods tested successfully.

--- src/main/java/com/example/restblog/web/PostsController.java
+++ src/main/java/com/example/restblog/web/PostsController.java
@@ -1,11 +1,46 @@
 package com.example.restblog.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.restblog.data.Post;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/posts", headers = "Accept=application/json")
 public class PostsController {
 
+    ArrayList<Post> posts = new ArrayList<>();
+    Post post1 = new Post(1L, "My First Post", "This is the body/contents of my first post.");
+    Post post2 = new Post(2L, "My Second Post", "This is the body/contents of my second post.");
+    Post post3 = new Post(3L, "My Third Post", "This is the body/contents of my third post.");
+
+
+    public ArrayList<Post> setPost(){
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+
+        return posts;
+    }
+
+
+    @GetMapping()
+    public ArrayList<Post> getAll() {
+        posts.removeAll(posts);
+        return setPost();
+    }
+
+    @GetMapping("{id}")
+    public Post getById(@PathVariable Long id) {
+        for (Post post : getAll()) {
+            if (id == post.getId()) {
+                return post;
+            }
+        }
+        return new Post();
+    }
+
 
 }
+
