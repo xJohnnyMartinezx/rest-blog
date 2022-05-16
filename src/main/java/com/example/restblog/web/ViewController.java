e7df4d297d7ab58a3c8e0481705ce912968dd42d
Johnny Martinez <mrtnz.jn@gmail.com>
2022-05-16 11:31:52 -0500

    Created web package and two controllers HelloController and ViewController.

---
+++ src/main/java/com/example/restblog/web/ViewController.java
@@ -0,0 +1,15 @@
+package com.example.restblog.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping({"/", "/about", "/posts", "/login", "/home"})
+    public String showView() {
+        return "forward:/index.html";
+    }
+}
+
